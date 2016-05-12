e8b15decea04e68d98a51a84e18065932441cc64
John Bannister <jbannister@airsci.com>
2016-05-12 16:04:44 -0700

    updated u* estimate script

--- R/emiss_functions.R
+++ R/emiss_functions.R
@@ -29,7 +29,6 @@ multi_read <- function(path, filenames){
   df1
 }
 
-
 #' Build reference list for cardinal direction angles.
 #' 
 #' @return A list of vectors for the 16 cardinal directions. 
@@ -49,7 +48,6 @@ direction_reference <- function(){
   dir_ref
 }
 
-
 #' Assign cardinal direction to wind direction.
 #' 
 #' Go through a data file containing wind directions (in angles) and assign one 
@@ -67,10 +65,58 @@ assign_cardinal <- function(df1, dir_column){
                      rep("E", 2), rep("ESE", 2), rep("SE", 2), rep("SSE", 2), 
                      rep("S", 2), rep("SSW", 2), rep("SW", 2), rep("WSW", 2), 
                      rep("W", 2), rep("WNW", 2), rep("NW", 2), rep("NNW", 2), 
-                     "N")
+                     rep("N", 2))
   f1 <- function(x) cardinal_dirs[findInterval(x, angles)]
   dir_name <- lapply(df1[ , dir_column], f1)
   dir_name <- unlist(dir_name)
   df1$wd.named <- dir_name
   df1
+}
+
+build_hourly_lm <- function(data, von.karman){
+df_sum <- data %>% group_by(deployment, datetime) %>%
+  do(model = lm(log.h ~ v, data=.), n = nrow(.), 
+     num.dirs = length(unique(.$wd.named)),
+     wd = unique(.$wd.named)) %>%
+  ungroup()
+df_sum$n <- unlist(df_sum$n)
+df_sum$num.dirs <- unlist(df_sum$num.dirs)
+df_sum <- filter(df_sum, num.dirs==1)
+df_sum$wd <- unlist(df_sum$wd)
+df_sum$intercept <- lapply(df_sum$model, function(x) coefficients(x)[1])
+df_sum$intercept <- unlist(df_sum$intercept)
+df_sum$slope <- lapply(df_sum$model, function(x) coefficients(x)[2])
+df_sum$slope <- unlist(df_sum$slope)
+df_sum$z_0 <- exp(df_sum$intercept)
+k <- von.karman
+df_sum$u_star <- k / df_sum$slope
+print(paste0(nrow(filter(df_sum, slope < 0)), 
+             " data points thrown out due to negative slope."))
+df_sum <- filter(df_sum, slope > 0)
+df_sum
+}
+
+build_average_table <- function(data){
+df_tbl <- data %>% group_by(deployment, wd) %>%
+  do(u_star.avg = mean(.$u_star), u_star.sd = sd(.$u_star),
+     z_0.avg = mean(.$z_0), z_0.sd = sd(.$z_0), n=nrow(.)) %>% ungroup()
+for (i in 1:nrow(df_tbl)){
+  df_tbl$angle[i] <- dir.ref[names(dir.ref)==df_tbl$wd[i]][[1]][3]
+}
+df_tbl$u_star.avg <- unlist(df_tbl$u_star.avg)
+df_tbl$u_star.sd <- unlist(df_tbl$u_star.sd)
+df_tbl$z_0.sd <- unlist(df_tbl$z_0.sd)
+df_tbl$z_0.avg <- unlist(df_tbl$z_0.avg)
+df_tbl$n <- unlist(df_tbl$n)
+df_tbl$angle <- unlist(df_tbl$angle)
+df_tbl
+}
+
+flip_wind <- function(vec){
+  vec_out <- vec
+  for (i in 1:length(vec)){
+    if (vec[i] >= 0 & vec[i] < 180) vec_out[i] <- vec[i] + 180
+    if (vec[i] >= 180 & vec[i] < 360) vec_out[i] <- vec[i] - 180
+  }
+    vec_out
 }
--- scripts/do.R
+++ scripts/do.R
@@ -7,106 +7,107 @@ load_all()
 library(dplyr)
 library(reshape2)
 library(ggplot2)
-library(gridExtra)
+library(lubridate)
 load("./data-clean/met_data.RData")
-
-# check for potential problem data points by looking at station battery voltage
-batt_check <- df1$batt > mean(df1$batt) - 4*sd(df1$batt) | 
-  df1$batt < mean(df1$batt) + 4*sd(df1$batt)
-print(paste0(sum(df1$batt < mean(df1$batt) - 4*sd(df1$batt) | 
-                 df1$batt > mean(df1$batt) + 4*sd(df1$batt)),
-             " data points with unusual battery voltage.")) 
-df1 <- df1[batt_check, ]
 
 ws_min <- 1 # low windspeed data cutoff
 dir_sd_max <- 11.25 / 2 # wind direction standard deviation cutoff
-dir_var_flag <- FALSE # should data be limited by directional variation?
+dir_sd_flag <- FALSE # should data be limited by directional variation?
 
-# eliminate all low wind speed data - hours with speed at lowest anemometer
-# less than 1 m/s
-df1 <- filter(df1, ws.1m > ws_min)
+# eliminate all low wind speed data - hours with speed at 2m anemometer
+# less than assigned minimum (above)
+df4 <- filter(df4, ws.2m > ws_min)
+
+df4[df4$deployment==1003 & df4$datetime < '2016-01-28 16:00', ]$wd.6m <- 
+   flip_wind(df4[df4$deployment==1003 & 
+            df4$datetime < '2016-01-28 16:00', ]$wd.6m)
+df4 <- assign_cardinal(df4, 3)
 
 # eliminate data hours with wind direction standard deviation greater than
 # 22.5deg (indicates swirling winds for that hour). 
-if (dir_var_flag==TRUE) df1 <- df1[df1$wd.6m.sd < dir_sd_max, ]
+if (dir_sd_flag==TRUE) df4 <- df4[df4$wd.sd.6m < dir_sd_max, ]
 
-df_melt <- df1 %>% 
-  melt(id.vars=c("timestamp", "site", "wd.named"), 
-       measure.vars=c("ws.6m", "ws.2m", "ws.1m"),
+names(df4)[names(df4)=="ws.15cm"] <- "ws.0m"
+df_all <- df4
+df_dec <- filter(df4, month(datetime)==12)
+all_melt <- df_all %>% 
+  melt(id.vars=c("datetime", "deployment", "wd.named"), 
+       measure.vars=c("ws.6m", "ws.2m", "ws.1m", "ws.0m"),
        value.name="v")
-df_melt$h <- as.numeric(substr(df_melt$variable, 4, 4))
-df_melt[df_melt$h==1, ]$h <- 0.15
-df_melt$log.h <- log(df_melt$h)
+all_melt$h <- as.numeric(substr(all_melt$variable, 4, 4))
+# lowest anemometer is a height 0.15m (not 0m)
+all_melt[all_melt$h==0, ]$h <- 0.15
+all_melt$log.h <- log(all_melt$h)
+dec_melt <- df_dec %>% 
+  melt(id.vars=c("datetime", "deployment", "wd.named"), 
+       measure.vars=c("ws.6m", "ws.2m", "ws.1m", "ws.0m"),
+       value.name="v")
+dec_melt$h <- as.numeric(substr(dec_melt$variable, 4, 4))
+# lowest anemometer is a height 0.15m (not 0m)
+dec_melt[dec_melt$h==0, ]$h <- 0.15
+dec_melt$log.h <- log(dec_melt$h)
 
-df_sum <- df_melt %>% group_by(site, timestamp) %>%
-  do(model = lm(log.h ~ v, data=.), n = nrow(.), 
-     num.dirs = length(unique(.$wd.named)),
-     wd = unique(.$wd.named)) %>%
-  ungroup()
-df_sum$n <- unlist(df_sum$n)
-df_sum$num.dirs <- unlist(df_sum$num.dirs)
-df_sum$wd <- unlist(df_sum$wd)
-df_sum$intercept <- lapply(df_sum$model, function(x) coefficients(x)[1])
-df_sum$intercept <- unlist(df_sum$intercept)
-df_sum$slope <- lapply(df_sum$model, function(x) coefficients(x)[2])
-df_sum$slope <- unlist(df_sum$slope)
-df_sum$z_0 <- exp(df_sum$intercept)
-k <- 0.4 # Von Karman's constant
-df_sum$u_star <- k / df_sum$slope
+all_hourly <- build_hourly_lm(all_melt, 0.4) %>%
+  select(datetime, deployment, wd, z_0, u_star) %>%
+  inner_join(select(df4, datetime, deployment, ws.6m, wd.6m), 
+             by=c("datetime", "deployment"))
+dec_hourly <- build_hourly_lm(dec_melt, 0.4) %>%
+  select(datetime, deployment, wd, z_0, u_star) %>%
+  inner_join(select(df4, datetime, deployment, ws.6m, wd.6m), 
+             by=c("datetime", "deployment"))
 
-df_sum <- filter(df_sum, n==3)
-df_sum <- filter(df_sum, num.dirs==1)
-print(paste0(nrow(filter(df_sum, slope < 0)), 
-             " data points thrown out due to negative slope."))
-df_sum <- filter(df_sum, slope > 0)
-
-df_tbl <- df_sum %>% group_by(site, wd) %>%
-  do(u_star.avg = mean(.$u_star), u_star.sd = sd(.$u_star),
-     z_0.avg = mean(.$z_0), z_0.sd = sd(.$z_0), n=nrow(.)) %>% ungroup()
 dir.ref <- direction_reference()
-for (i in 1:nrow(df_tbl)){
-  df_tbl$angle[i] <- dir.ref[names(dir.ref)==df_tbl$wd[i]][[1]][3]
-}
-df_tbl$u_star.avg <- unlist(df_tbl$u_star.avg)
-df_tbl$u_star.sd <- unlist(df_tbl$u_star.sd)
-df_tbl$z_0.sd <- unlist(df_tbl$z_0.sd)
-df_tbl$z_0.avg <- unlist(df_tbl$z_0.avg)
-df_tbl$n <- unlist(df_tbl$n)
-df_tbl$angle <- unlist(df_tbl$angle)
 
-for (i in unique(df_tbl$site)){
-  p_u <- df_tbl %>% filter(site==i) %>%
-    ggplot(aes(x = angle, y = u_star.avg)) +
-    geom_bar(stat='identity', color="black") + 
-    scale_x_continuous(breaks=unlist(lapply(dir.ref, function(x) x[[3]])),
-                       labels = waiver()) +
-    ylim(0, max(df_tbl$u_star.avg)) +
-#    coord_polar(start=-11.25*2*pi/360) +
-    ggtitle(paste0(i, " u*")) + xlab("") + ylab("u*") 
-  p_z <- df_tbl %>% filter(site==i) %>%
-    ggplot(aes(x = angle, y = z_0.avg)) +
-    geom_bar(stat='identity', color="black") + 
-    scale_x_continuous(breaks=unlist(lapply(dir.ref, function(x) x[[3]])),
-                       labels = waiver()) +
-    ylim(0, max(df_tbl$z_0.avg)) +
- #   coord_polar(start=-11.25*2*pi/360) +
-    geom_errorbar(aes(ymax=z_0.avg + z_0.sd, 
-                      ymin=ifelse(z_0.avg - z_0.sd>0, z_0.avg-z_0.sd, 0),
-                      color="+/- sd"), width=10) +
-    geom_label(aes(label=n, fill="# of data points")) +
-    labs(fill="", color="") + 
-    ggtitle(paste0(i, " z0")) + xlab("") + ylab("z0") 
-  assign(paste0("p", i, "_u"), p_u)
-  assign(paste0("p", i, "_z"), p_z)
-}
-grid.arrange(p1002_z, p1003_z, p1004_z, 
-             ncol=1, nrow=3, 
-             top=paste0("Salton Met Stations\n",
-                        "1m wind speed > ", ws_min, " m/s\n",
-                        "Hourly Direction Variance Filter = ", 
-                        as.character(dir_var_flag))) 
+all_tbl <- build_average_table(all_hourly)
+dec_tbl <- build_average_table(dec_hourly)
 
-p1 <- df_melt %>% filter(site==1003, wd.named=="W") %>%
-  ggplot(aes(x=v, y=log.h, color=timestamp)) + 
-  geom_path() +
-  geom_point()
+path <- "~/dropbox/Salton Sea CALPUFF/toKent/"
+write.csv(all_hourly, file=paste0(path, "all_hourly_ustar.csv"))
+write.csv(dec_hourly, file=paste0(path, "dec_hourly_ustar.csv"))
+write.csv(dec_tbl, file=paste0(path, "dec_directional_ustar.csv"))
+write.csv(all_tbl, file=paste0(path, "all_directional_ustar.csv"))
+
+p_bar <- df_tbl %>% 
+  ggplot(aes(x = angle, y = z_0.avg)) +
+  geom_point() +
+  scale_x_continuous(breaks=unlist(lapply(dir.ref, function(x) x[[3]])),
+                     labels = waiver()) +
+  facet_grid(deployment ~ ., scales="free_y") +
+  geom_errorbar(aes(ymax=z_0.avg + z_0.sd, 
+                    ymin=ifelse(z_0.avg - z_0.sd>0, z_0.avg-z_0.sd, 0),
+                  color="+/- sd"), width=10) 
+
+p_rose_all <- all_tbl %>% 
+  ggplot(aes(x = angle, y = u_star.avg)) +
+  geom_bar(stat='identity', color="black") + 
+  scale_x_discrete(breaks=unlist(lapply(dir.ref, function(x) x[[3]])),
+                   labels = waiver(), 
+                   drop=FALSE) +
+  coord_polar(start=-11.25*2*pi/360) +
+  facet_grid(deployment ~ ., scales="free_y") +
+  ggtitle("All Available Met Data") + xlab("") + ylab("u*") 
+png(filename=paste0(path, "all_met_ustar_rose.png"), height=9, width=4,
+    units="in", res=300)
+print(p_rose_all)
+dev.off()
+  
+p_rose_dec <- dec_tbl %>% 
+  ggplot(aes(x = angle, y = u_star.avg)) +
+  geom_bar(stat='identity', color="black") + 
+  scale_x_discrete(breaks=unlist(lapply(dir.ref, function(x) x[[3]])),
+                   labels = waiver(), 
+                   drop=FALSE) +
+  coord_polar(start=-11.25*2*pi/360) +
+  facet_grid(deployment ~ ., scales="free_y") +
+  ggtitle("December Met Data") + xlab("") + ylab("u*") 
+png(filename=paste0(path, "dec_met_ustar_rose.png"), height=9, width=4,
+    units="in", res=300)
+print(p_rose_dec)
+dev.off()
+
+#j <- "SW"
+#p1 <- df_melt %>% filter(deployment==1001, wd.named==j) %>% arrange(h) %>%
+#  ggplot(aes(x=v, y=log.h, group=datetime)) +
+#  geom_path() +
+#  geom_point(aes(color=datetime))
+
